4adf407ec0dbf7e02623078cd2ef5d516b024e74
Leanderrr <leanderdk@gmail.com>
2017-01-17 16:57:47 +0100

    w1d2 Updated FFT plot

--- PROCESS.md
+++ PROCESS.md
@@ -14,34 +14,84 @@ Process book for the programming minor eindproject.
 
 ## Week 1, day 4. 2017-1-12
 
+Planned:
 - Take microphone with me, test selection of two audio sources
 - Save microphone recording
 - Fix gain of audio
- Fast Fourier Transform audio signal
- Time Fast Fourier Transform
+Reality: 
+- Took microphone with me, selection audio source seems impossible to implement.
+- Fast Fourier Transform audio signal
 
 
 ## Week 1, day 5. 2017-1-13
 
-- (vx) Fast Fourier Transform audio signal
-- (vx) Time Fast Fourier Transform
-We put almost all our time into the presentation
+Planned:
+- Fast Fourier Transform audio signal
+- Time Fast Fourier Transform
+Reality:
+- We put almost all our time into the presentation
+- Timing doesn't seem to be necissary.
 
 ## Week 2, day 1. 2017-1-16
 
+Planned:
 - Plot buffer output
-- Plot FFT
+Reality:
+- Plotted buffer output!
+- Plotted FFT results
 
 
 ## Week 2, day 2. 2017-1-17
 
-Change Plot FFT
+Planned:
+- Change Plot FFT
+Reality:
+- Looked into data represented in FFT plot. 
+- Log transformed x-axis.
+- Calculated note name frequencies.
+- Put note name labels on correct place in FFT plot.
+- Looked into clearing up FFT results with gain.
+
 
 ## Week 2, day 3. 2017-1-18
 
-change Plot FFT
+Planned:
+change Plot FFT, Stack plots.
+Save FFT into matrix and plot heatmap.
 
 ## Week 2, day 4. 2017-1-16
 
+Planned:
+Plot heatmap 
+Add interactivity sliders (gain, zoom, analysis-time)
+
 ## Week 2, day 5. 2017-1-17
 
+Planned:
+- Add interactivity sliders (gain, zoom, analysis-time)
+
+## Week 3, day 1.
+
+Planned:
+- Add interactivity tick (smoothed view)
+- Implement spiral FFT plot.
+- Add buttons to switch plot type.
+
+## Week 3, day 2.
+
+Planned:
+- Spiral FFT plot.
+
+
+## Week 3, day 3.
+
+Planned: 
+- Spiral FFT plot.
+
+## Week 3, day 4.
+
+## Week 3, day 5.
+
+## Week 4, day 1.
+
+## Week 4, day 2
--- prototype 2/microphone_input.js
+++ prototype 2/microphone_input.js
@@ -1,14 +1,39 @@
 
-var BUFF_SIZE = 2**13;
+var BUFF_SIZE = 2**12;
 
 
+// Create Frequency labels
+var notesN = 88 // number of labels to make
+	noteNames = []
+	noteNamesExample = ["A", "", "B", "C", "", "D", "", "E", "F", "", "G", ""]
+	noteFreqs = [27.5] // A0 = 27.5Hz
+	interval = Math.pow(2,1/12) // A half step is 12th root of 2 increase in frequency
+	octaveLength = noteNamesExample.length
+	octaveN = 0;
+for (var i = 0; i < notesN; i++){
+	// Add frequency i 
+	freqi = noteFreqs[noteFreqs.length-1]*interval;
+	noteFreqs.push(freqi);
+	
+	// Add name i
+	nami = noteNamesExample[i % octaveLength];
+	if (nami == "C"){
+		octaveN += 1;
+	};
+	if (nami != ""){
+		nami = nami + octaveN;
+	};
+	noteNames.push(nami)
+};
+
+// ----------  PLOTTING FUNCTIONS -----------
 // INITIALIZE RAW SIGNAL PLOT
 function init_raw_plot(maxX) {
-	var width = 500
+	var width = 600
 		height = 170
 		margin = {top: 10, left: 50, bottom: 60, right: 30};
 	
-	var svg1 = d3.select('body').append('svg')
+	var svg = d3.select('body').append('svg')
 			.attr('width',width)
 			.attr('height',height)
 			.attr('id','raw_line_svg')
@@ -31,7 +56,7 @@ function init_raw_plot(maxX) {
 		.orient("left");
 		
 	// Add x axis with label
-	svg1.append("g")
+	svg.append("g")
 	  .attr("id","x-axis")
 	  .attr("transform", "translate(0," + ((height-margin.bottom)-margin.top) + ")")
 	  .call(xAxis)
@@ -39,13 +64,13 @@ function init_raw_plot(maxX) {
 		//.attr("transform", "translate(0, " + -height + ")")
 		.attr("y", 25)
 		.attr("dy", ".71em")
-		.attr("x", width-margin.right)
+		.attr("x", width-margin.right*2)
 		.style("text-anchor", "end")
 		.style("font-size", "15px")
 		.text("time (ms)");
 	
 	// Add y axis with label
-	svg1.append("g")
+	svg.append("g")
 	  .attr("id","y-axis")
 	  .call(yAxis)
 	.append("text")
@@ -57,16 +82,16 @@ function init_raw_plot(maxX) {
 		.style("font-size", "15px")
 		.text("input");
 		
-	return {"svg":svg1,"xAxis":xAxis,"yAxis":yAxis,"xScale":xScale,"yScale":yScale}
+	return {"svg":svg,"xAxis":xAxis,"yAxis":yAxis,"xScale":xScale,"yScale":yScale}
 }
 
 // INITIALIZE FFT PLOT
-function init_FFT_plot(maxX) {
-	var width = 500
+function init_FFT_plot(Xlim) {
+	var width = 600
 		height = 500
 		margin = {top: 10, left: 50, bottom: 60, right: 30};
 	
-	var svg2 = d3.select('body').append('svg')
+	var svg = d3.select('body').append('svg')
 			.attr('width',width)
 			.attr('height',height)
 			.attr('id','FFT_svg')
@@ -76,12 +101,14 @@ function init_FFT_plot(maxX) {
 				.attr('height',height - margin.top - margin.bottom)
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 				
-	var xScale = d3.scale.linear().domain([0, maxX]).range([0, width - margin.left - margin.right])
-		yScale = d3.scale.linear().domain([0, 256]).range([height - margin.top - margin.bottom, 0]);
+	var xScale = d3.scale.log().domain(Xlim).range([0, width - margin.left - margin.right])
+		yScale = d3.scale.linear().domain([1, 300]).range([height - margin.top - margin.bottom, 0]);
 
 	// Axis properties
 	var xAxis = d3.svg.axis()
 		.scale(xScale)
+		.tickValues(noteFreqs)
+		.tickFormat(function(d,i){if (i % 12 == 3){ return noteNames[i]}})
 		.orient("bottom");
 	var yAxis = d3.svg.axis()
 		.ticks(3)
@@ -89,7 +116,7 @@ function init_FFT_plot(maxX) {
 		.orient("left");
 		
 	// Add x axis with label
-	svg2.append("g")
+	svg.append("g")
 	  .attr("id","x-axis")
 	  .attr("transform", "translate(0," + ((height-margin.bottom)-margin.top) + ")")
 	  .call(xAxis)
@@ -97,13 +124,13 @@ function init_FFT_plot(maxX) {
 		//.attr("transform", "translate(0, " + -height + ")")
 		.attr("y", 25)
 		.attr("dy", ".71em")
-		.attr("x", width-margin.right)
+		.attr("x", width-margin.right*2)
 		.style("text-anchor", "end")
 		.style("font-size", "15px")
-		.text("frequency (bin?)");
+		.text("frequency (note)");
 	
 	// Add y axis with label
-	svg2.append("g")
+	svg.append("g")
 	  .attr("id","y-axis")
 	  .call(yAxis)
 	.append("text")
@@ -115,12 +142,55 @@ function init_FFT_plot(maxX) {
 		.style("font-size", "15px")
 		.text("amplitude");
 		
-	return {"svg":svg2,"xAxis":xAxis,"yAxis":yAxis,"xScale":xScale,"yScale":yScale}
+	return {"svg":svg,"xAxis":xAxis,"yAxis":yAxis,"xScale":xScale,"yScale":yScale}
 }
 
-//obj.key ====== obj['key']
+// Plot the raw input
+function plot_line(time, given_typed_array){
+	var line = d3.svg.line()
+		.x(function(d, i) {
+			return svg1.xScale(time[i])
+		})
+		.y(function(d, i) {
+			return svg1.yScale(given_typed_array[i]-(256/2))
+		})
+		svg1.svg.select("#raw_line").remove()
+		svg1.svg.append("path")
+		  .data([time, given_typed_array])
+		  .attr("class", "line")
+		  .attr("id", "raw_line")
+		  .attr("d", line);
+	//console.log(label);
+	//console.log(given_typed_array);
+}
 
-// AUDIO START
+// Plot the FFT
+function plot_FFT(freq_bins, freq_values){
+	var  area = d3.svg.area()
+		.x(function(d, i) {
+			return svg2.xScale(freq_bins[i])
+		})
+		.y0(height-margin.bottom-margin.top)
+		.y1(function(d, i) {
+			//console.log(freq_values[i])
+			vali = freq_values[i]-50
+			if (vali < 0){
+				return(height-margin.bottom-margin.top)}
+			else{
+				return svg2.yScale(vali)
+			}
+		})
+		svg2.svg.select("#FFT_line").remove()
+		svg2.svg.append("path")
+		  .data([freq_bins, freq_values])
+		  .attr("class", "area")
+		  .attr("id", "FFT_line")
+		  .attr("d", area);
+		  //console.log("number of freq bins: " + freq_bins.length);
+		  //console.log("number of freq vals: " + freq_values.length);
+}
+
+// ------------ AUDIO START ---------------
 var webaudio_tooling_obj = function () {
 	
 	// The audio object
@@ -134,14 +204,20 @@ var webaudio_tooling_obj = function () {
 	for  (var i = 1; i <= BUFF_SIZE; i++) {
 	   time.push(i/sRate*1000);
 	}
-	for  (var i = 1; i <= BUFF_SIZE/2; i++) {
-	   freqbins.push(i * sRate/BUFF_SIZE);
+	for  (var i = 1; i <= BUFF_SIZE/4; i++) {
+	   freqbins.push(Math.round(i * sRate/BUFF_SIZE));
 	}
+	
 	// Create SVGs for plots
 	svg1 = init_raw_plot(time[time.length-1]);
-	svg2 = init_FFT_plot(freqbins[freqbins.length-1]);
-
+	svg2 = init_FFT_plot([freqbins[0],freqbins[freqbins.length-1]]);
+	
     // Print some stuff
+	console.log("number of notes in octave: " + octaveLength);
+	//console.log(noteFreqs);
+	//console.log(noteNames);
+	console.log("interval = " + interval)
+	console.log(freqbins)
 	console.log("buffer size   = " + BUFF_SIZE);
 	console.log("buffer length = " + Math.round(time[time.length-1]) + " ms");
 	console.log("sample rate   = " + sRate + " Hz")
@@ -171,48 +247,11 @@ var webaudio_tooling_obj = function () {
             );
     } else { alert('getUserMedia not supported in this browser.'); }
 
-	// Plot the raw input
-    function plot_line(time, given_typed_array){
-		var line = d3.svg.line()
-			.x(function(d, i) {
-				return svg1.xScale(time[i])
-			})
-			.y(function(d, i) {
-				return svg1.yScale(given_typed_array[i]-(256/2))
-			})
-			svg1.svg.select("#raw_line").remove()
-			svg1.svg.append("path")
-			  .data([time, given_typed_array])
-			  .attr("class", "line")
-			  .attr("id", "raw_line")
-			  .attr("d", line);
-		//console.log(label);
-		//console.log(given_typed_array);
-    }
-	
-	function plot_FFT(freq_bins, freq_values){
-		var  line = d3.svg.line()
-			.x(function(d, i) {
-				return svg2.xScale(freq_bins[i])
-			})
-			.y(function(d, i) {
-				//console.log(freq_values[i])
-				return svg2.yScale(freq_values[i])
-			})
-			svg2.svg.select("#FFT_line").remove()
-			svg2.svg.append("path")
-			  .data([freq_bins, freq_values])
-			  .attr("class", "line")
-			  .attr("id", "FFT_line")
-			  .attr("d", line);
-			  console.log("number of freq bins: " + freq_bins.length);
-			  console.log("number of freq vals: " + freq_values.length);
-	}
 
-    function process_microphone_buffer(event) {
-        var i, N, inp, microphone_output_buffer;
-        microphone_output_buffer = event.inputBuffer.getChannelData(0); // just mono - 1 channel for now
-    }
+    //function process_microphone_buffer(event) {
+    //   var i, N, inp, microphone_output_buffer;
+    //    microphone_output_buffer = event.inputBuffer.getChannelData(0); // just mono - 1 channel for now
+    //}
 	
 		
     function start_microphone(stream){
@@ -221,12 +260,12 @@ var webaudio_tooling_obj = function () {
         gain_node.connect( audioContext.destination );
 
         microphone_stream = audioContext.createMediaStreamSource(stream);
-        microphone_stream.connect(gain_node); 
+        //microphone_stream.connect(gain_node); 
 
-        script_processor_node = audioContext.createScriptProcessor(BUFF_SIZE, 1, 1);
-        script_processor_node.onaudioprocess = process_microphone_buffer;
+        //script_processor_node = audioContext.createScriptProcessor(BUFF_SIZE, 1, 1);
+        //script_processor_node.onaudioprocess = process_microphone_buffer;
 
-        microphone_stream.connect(script_processor_node);
+        //microphone_stream.connect(script_processor_node);
 
         // --- enable volume control for output speakers
 
@@ -252,7 +291,7 @@ var webaudio_tooling_obj = function () {
 
         var buffer_length = analyser_node.frequencyBinCount;
 
-        var array_freq_domain = new Uint8Array(buffer_length);
+        var array_freq_domain = new Uint8Array(buffer_length/2);
         var array_time_domain = new Uint8Array(BUFF_SIZE);
 
         script_processor_analysis_node.onaudioprocess =  function() {
